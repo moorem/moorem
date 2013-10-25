c6245287f3a02b4b3add4c1533fb081b1f955515
thiyagarajan <thiyagarajannv@yahoo.com>
2013-10-25 18:35:53 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moorem::Application.config.secret_key_base = '13f78580f7a8f551dfa85997c48246a8a7e1f03e1032867df6ab1033d8cde4f7fe6eca59116542ba79c5becd63f7123187eaba75db29dcb4ffb59e43f7619501'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Moorem::Application.config.session_store :cookie_store, key: '_moorem_session'
