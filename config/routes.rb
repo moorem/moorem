956015fe528eec3ab04751d4b915b1fed1a3bb1d
thiyagarajan <thiyagarajannv@yahoo.com>
2013-11-01 21:15:15 +0530

    added contact form

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Moorem::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'pages#contact_us'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
