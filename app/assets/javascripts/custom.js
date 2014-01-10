9a7044c819f3547a242156118a6d239a535bdd27
PavitraKumarSingh <pavitraatwork@gmail.com>
2014-01-10 22:19:27 +0530

    slider issue fixes for production

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -325,12 +325,6 @@ $(document).ready(function(){
 			social_tools: '<div class="pp_social"><div class="twitter"><a href="http://twitter.com/share" class="twitter-share-button" data-count="none">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></div><div class="facebook"><iframe src="http://www.facebook.com/plugins/like.php?locale=en_US&href='+location.href+'&amp;layout=button_count&amp;show_faces=true&amp;width=500&amp;action=like&amp;font&amp;colorscheme=light&amp;height=23" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:500px; height:23px;" allowTransparency="true"></iframe></div></div>' /* html or false to disable */
 
  });
-jQuery('.portfolio-carousel').carousel({interval: false, wrap: false});
-	
-	
-	jQuery('.client-carousel').carousel({interval: false, wrap: false});
-	
-	jQuery('.testimonials-carousel').carousel({interval: 5000, pause: "hover"});
 
 	});
 
