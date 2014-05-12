4b45683b28f7f212344285ce15916107c11139f0
Rahul <rahul@moorem.com>
2014-05-12 20:07:42 +0530

    design and mailer email fixed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,6 @@
 class UserMailer < ActionMailer::Base
   default from: 'contact@moorem.com'
 
-
   def contact(con)
     @con = con
     mail(to: @con.email, subject: 'Thanks for contacting us, will get back shortly')
